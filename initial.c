8ac6aaf3f34f07578655c9c6f0fff8afaf97cc00
Arik Jahan Noor <arikjahannoor@gmail.com>
2025-12-09 00:49:23 +0600

    add the c file initialing

---
+++ initial.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the value for running the loop\n");
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        printf("Hello world\n the working is going well\n");
+    }
+    return 0;
+}
